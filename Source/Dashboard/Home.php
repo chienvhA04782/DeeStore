9ae16b826e7527b2332c91f4a8ff58209398b5a3
chienvu <fchienvuhoang@gmail.com>
2013-07-03 12:36:33 +0700

    add folder dashboard

--- Source/Dashboard/Home.php
+++ Source/Dashboard/Home.php
@@ -5,18 +5,20 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>DEESTORE DASHBOARD</title>
         <link href="../Media/Lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-        <link href="Media/Layer.css" rel="stylesheet" />
+        <link href="Media/Css/Layer.css" rel="stylesheet" />
         <script src="../Media/JavaScript/jquery/jquery.min.js"></script>
         <script src="../Media/Lib/bootstrap/js/bootstrap.min.js"></script>
     </head>
     <body style="background-color: #404040">
         <?php
         // put your code here
+        require './Model/Connect.php';
         ?>
         <header>
         </header>
         <div class="container">
             <div class="span12" style="background-color: #fff; min-height: 700px">
+
             </div>
         </div>
         <footer>
--- Source/Index.php
+++ Source/Index.php
@@ -70,27 +70,6 @@
                     </div>
                 </div>
             </div>
-            <script src="/Media/JavaScript/angular/angular.min.js"></script>
-            <script src="/Media/JavaScript/angulartics.js"></script>
-            <script src="/Media/JavaScript/angulartics-ga.js"></script>
-            <script>
-                (function(i, s, o, g, r, a, m) {
-                    i['GoogleAnalyticsObject'] = r;
-                    i[r] = i[r] || function() {
-                        (i[r].q = i[r].q || []).push(arguments)
-                    }, i[r].l = 1 * new Date();
-                    a = s.createElement(o),
-                            m = s.getElementsByTagName(o)[0];
-                    a.async = 1;
-                    a.src = g;
-                    m.parentNode.insertBefore(a, m)
-                })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-                ga('create', 'UA-10255892-8', 'luisfarzati.github.io');
-                ga('send', 'pageview'); // <-- DELETE THIS LINE!
-            </script>
-
-            <script src="/Media/JavaScript/application.js"></script>
             <?php
             include './Share/Footer.php';
             ?>
@@ -144,5 +123,26 @@
             // load product with categories
         }
         ?>
+
+        <script src="/Media/JavaScript/angular/angular.min.js"></script>
+        <script src="/Media/JavaScript/angulartics.js"></script>
+        <script src="/Media/JavaScript/angulartics-ga.js"></script>
+        <script>
+            (function(i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function() {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+            ga('create', 'UA-10255892-8', 'luisfarzati.github.io');
+            ga('send', 'pageview'); // <-- DELETE THIS LINE!
+        </script>
+        <script src="/Media/JavaScript/application.js"></script>
     </body>
 </html>
