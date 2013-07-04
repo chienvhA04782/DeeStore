59781cff3a24e9e23a1e9cf031da3893b6a28fd0
chienvu <fchienvuhoang@gmail.com>
2013-07-05 00:25:07 +0700

    update h3 description for ProductDetails.php

--- Source/ProductDetails.php
+++ Source/ProductDetails.php
@@ -23,61 +23,61 @@
     </head>
     <style> body{background-color: #FFF} </style>
     <body data-status="{{ status }}">
-    <div id="page">
-        <?php
-        include './Share/Header.php';
-        ?>
-        <div class="bodyContent">
-            <div class='Inside'>
-                <div class="left">
-                    <div class="titleCate">
-                        <h2 style="color: #dc2606">DEESTORE SHOP</h2>
-                        <?php
-                        include './Share/LeftMenu.php';
-                        ?>
+        <div id="page">
+            <?php
+            include './Share/Header.php';
+            ?>
+            <div class="bodyContent">
+                <div class='Inside'>
+                    <div class="left">
+                        <div class="titleCate">
+                            <h2 style="color: #dc2606">DEESTORE SHOP</h2>
+                            <?php
+                            include './Share/LeftMenu.php';
+                            ?>
+                        </div>
+                        <div class="titleCate" title="giới thiệu" style="margin-top: 20px">
+                            <?php
+                            include './About.php';
+                            ?>
+                        </div>
                     </div>
-                    <div class="titleCate" title="giới thiệu" style="margin-top: 20px">
-                        <?php
-                        include './About.php';
-                        ?>
-                    </div>
-                </div>
-                <div class="right">
-                    <div class="boundDetails">
-                        <?php
-                        $product = new Product();
-                        $resultDetailsProduct = $product->FetchProductByProductId($pId);
-                        while ($rowDetails = mysqli_fetch_array($resultDetailsProduct)) {
-                            echo '<div class="warpperDetails">
+                    <div class="right">
+                        <div class="boundDetails">
+                            <?php
+                            $product = new Product();
+                            $resultDetailsProduct = $product->FetchProductByProductId($pId);
+                            while ($rowDetails = mysqli_fetch_array($resultDetailsProduct)) {
+                                echo '<div class="warpperDetails">
                                 <div class="leftDetails">
                                     <h2 class="titleDetails" title="' . $rowDetails["ProductName"] . '">' . $rowDetails["ProductName"] . '</h2>
-                                    <div class="description" title="' . $rowDetails["ProductDescription"] . '">
+                                    <div class="description" title="' . '"><h3 title="' . $rowDetails["ProductDescription"] . '">
                                     ' . $rowDetails["ProductDescription"] . '
-                                    </div>
+                                    </h3></div>
                                     <div class="inforProduct">
                                         <div class="size">
                                             <label>SIZE:</label> ';
 
-                            $joinProduct = new JoinProductSize();
-                            $resultJoinProduct = $joinProduct->FetchJoinProductSizeByProductID($pId);
-                            while ($row = mysqli_fetch_array($resultJoinProduct)) {
-                                $productSize = new ProductSize();
-                                $resultSize = $productSize->FetchProductSizeByProductSizeId($row["ProductSizeID"]);
-                                while ($rowSize = mysqli_fetch_array($resultSize)) {
-                                    echo '<a href="">' . $rowSize["ProductSizeNumber"] . '</a>';
-                                    break;
+                                $joinProduct = new JoinProductSize();
+                                $resultJoinProduct = $joinProduct->FetchJoinProductSizeByProductID($pId);
+                                while ($row = mysqli_fetch_array($resultJoinProduct)) {
+                                    $productSize = new ProductSize();
+                                    $resultSize = $productSize->FetchProductSizeByProductSizeId($row["ProductSizeID"]);
+                                    while ($rowSize = mysqli_fetch_array($resultSize)) {
+                                        echo '<a href="">' . $rowSize["ProductSizeNumber"] . '</a>';
+                                        break;
+                                    }
                                 }
-                            }
-                            echo'  </div>
+                                echo'  </div>
                                 <div class="price">';
-                            if (isset($rowDetails['ProductPriceOld']) && !empty($rowDetails['ProductPriceOld'])) {
-                                echo '<label class="oldPrice">' . number_format($rowDetails['ProductPriceOld'], 0, '.', '.') . ' VNĐ</label>';
-                            }
-                            if (isset($rowDetails['ProductPriceCurrent']) && !empty($rowDetails['ProductPriceCurrent'])) {
-                                echo '<label class="currentPrice">' . number_format($rowDetails['ProductPriceCurrent'], 0, '.', '.') . ' VNĐ</label>';
-                            }
-                            echo '</div></div>';
-                            echo '</div>
+                                if (isset($rowDetails['ProductPriceOld']) && !empty($rowDetails['ProductPriceOld'])) {
+                                    echo '<label class="oldPrice">' . number_format($rowDetails['ProductPriceOld'], 0, '.', '.') . ' VNĐ</label>';
+                                }
+                                if (isset($rowDetails['ProductPriceCurrent']) && !empty($rowDetails['ProductPriceCurrent'])) {
+                                    echo '<label class="currentPrice">' . number_format($rowDetails['ProductPriceCurrent'], 0, '.', '.') . ' VNĐ</label>';
+                                }
+                                echo '</div></div>';
+                                echo '</div>
                                 <div class="rightDetails">
                                     <div class="contentImageSLide">
                                         <div class="mainImgSlide">
@@ -85,12 +85,12 @@
                                         </div>
                                         <div class="slideSmalIcon">
                                             <div class="smallImg">';
-                            $ProductGallery = new ProductGallery();
-                            $resultGallery = $ProductGallery->FetchProductGalleryByProductId($pId);
-                            while ($row2 = mysqli_fetch_array($resultGallery)) {
-                                echo "<img src=" . $row2['ProductGalleryPath'] . ">" . "</img>";
-                            }
-                            echo '</div>
+                                $ProductGallery = new ProductGallery();
+                                $resultGallery = $ProductGallery->FetchProductGalleryByProductId($pId);
+                                while ($row2 = mysqli_fetch_array($resultGallery)) {
+                                    echo "<img src=" . $row2['ProductGalleryPath'] . ">" . "</img>";
+                                }
+                                echo '</div>
                                         </div>
                                     </div>
                                 </div>
@@ -98,7 +98,7 @@
                                     <h2 class="titleShip">SẢN PHẨM CÙNG LOẠI</h2>
                                     <div class="contentProductShow" style="border: none">
                                         <div class="productContent"> ';
-                            echo '<div class="items">
+                                echo '<div class="items">
                                                 <div class="sizeH">
                                                     <label>
                                                         SIZE: X | XL | 1 | 2A
@@ -133,19 +133,19 @@
                                     </div>
                                 </div>
                             </div>';
-                            break;
-                        }
-                        ?>
-                    </div>                       
-                    <div ng-view> 
+                                break;
+                            }
+                            ?>
+                        </div>                       
+                        <div ng-view> 
 
+                        </div>
                     </div>
                 </div>
             </div>
+            <?php
+            include './Share/Footer.php';
+            ?>
         </div>
-        <?php
-        include './Share/Footer.php';
-        ?>
-    </div>
-</body>
+    </body>
 </html>
